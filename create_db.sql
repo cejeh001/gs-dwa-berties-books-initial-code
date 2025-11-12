068bc3c97e543081fa1539590cdd6f00d28bf067
Mel0nWater <chib.ejehu2@gmail.com>
2025-11-12 02:14:47 +0000

    `Added application user creation to create_db.sql`

--- create_db.sql
+++ create_db.sql
@@ -4,6 +4,10 @@
 CREATE DATABASE IF NOT EXISTS berties_books;
 USE berties_books;
 
+# Create the application user
+CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
+GRANT ALL PRIVILEGES ON berties_books.* TO 'berties_books_app'@'localhost';
+
 # Create the tables
 CREATE TABLE IF NOT EXISTS books (
     id     INT AUTO_INCREMENT,
@@ -11,6 +15,4 @@ CREATE TABLE IF NOT EXISTS books (
     price  DECIMAL(5, 2),
     PRIMARY KEY(id));
 
-# Create the application user
-CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
-GRANT ALL PRIVILEGES ON berties_books.* TO ' berties_books_app'@'localhost';
+
